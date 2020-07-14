b6080d01dfd75d393bef53e168b253723273c622
tinotenda6 <59712043+tinotenda6@users.noreply.github.com>
2020-07-13 19:23:01 -0500

    project is done

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
