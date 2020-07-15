c34fa4656bad13a0ce420868790eac45a5612e6a
tinotenda6 <59712043+tinotenda6@users.noreply.github.com>
2020-07-15 12:27:49 -0500

    changed the password

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb2',
         'USER': 'postgres',
-        'PASSWORD': 'Tinokudai12',
+        'PASSWORD': '*******',
         'HOST': 'localhost',
         'PORT': '5432',
     }
